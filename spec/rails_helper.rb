9ca3f9071d90d746e394082216d26a2247bbaf5a
Lizzie Sorin <lizzie.sorin@hotmail.fr>
2017-05-19 17:06:02 +0000

    Done.

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -19,6 +19,7 @@ Dir[Rails.root.join("spec/support/**/*.rb")].each { |f| require f }
 ActiveRecord::Migration.maintain_test_schema!
 
 RSpec.configure do |config|
+  config.color = true
   # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
   config.fixture_path = "#{::Rails.root}/spec/fixtures"
 
@@ -46,6 +47,6 @@ RSpec.configure do |config|
   config.after(:each) do
     DatabaseCleaner.clean
   end
-  
+
   config.infer_spec_type_from_file_location!
 end
