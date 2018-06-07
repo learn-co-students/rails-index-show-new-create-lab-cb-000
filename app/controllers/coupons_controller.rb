12e84e81f0f9bea58a02948a20551065de85da1c
“Alyssa <clairaly13@gmail.comgit config --global user.name clairaly>
2018-06-07 10:42:52 -0400

    Done.

--- app/controllers/coupons_controller.rb
+++ app/controllers/coupons_controller.rb
@@ -2,11 +2,14 @@ class CouponsController < ApplicationController
   def index
     @coupons = Coupon.all
   end
+
   def new
   end
+
   def show
     @coupon = Coupon.find(params[:id])
   end
+
   def create
     @coupon = Coupon.new
     @coupon.coupon_code = params[:coupon_code]
