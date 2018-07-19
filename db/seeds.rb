e40336a9c92d8df1b91ea5fd1ef116177ec49424
nichia <nichia@gmail.com>
2018-07-19 13:29:36 -0400

    Done.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+Coupon.create(coupon_code: 'JULY08', store: 'Macys')
+Coupon.create(coupon_code: 'FUN', store: 'Macys')
+Coupon.create(coupon_code: 'FIREWORKS', store: 'Home Depot')
+Coupon.create(coupon_code: 'RAIN', store: 'Champs Sport')
