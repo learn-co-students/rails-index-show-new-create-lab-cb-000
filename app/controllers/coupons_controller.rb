96091431344e2e7326bc0b5d0d641f0a2b36f8a7
Sergio Marrocoli <s.marrocoli@gmail.com>
2019-02-20 11:00:39 +0000

    Done.

--- app/controllers/coupons_controller.rb
+++ app/controllers/coupons_controller.rb
@@ -6,10 +6,13 @@ class CouponsController < ApplicationController
     @coupons = Coupon.all
     # binding.pry
   end
-  
+
   def create
     @coupon = Coupon.create(coupon_code: params[:coupon_code], store: params[:store])
     redirect_to coupon_path(@coupon)
+  end
+
+  def new
   end
 
   def show
